0190c1d522f6bf8b24d8af46f26aa138b799317f
Hotdog <1963335234@qq.com>
2021-04-08 22:54:43 +0800

    123

---
+++ pages/page/user/user.js
@@ -0,0 +1,40 @@
+Page({
+    // data: {
+    //   text: "This is page data."
+    // },
+    // onLoad: function(options) {
+    //   // 页面创建时执行
+    // },
+    // onShow: function() {
+    //   // 页面出现在前台时执行
+    // },
+    // onReady: function() {
+    //   // 页面首次渲染完毕时执行
+    // },
+    // onHide: function() {
+    //   // 页面从前台变为后台时执行
+    // },
+    // onUnload: function() {
+    //   // 页面销毁时执行
+    // },
+    // onPullDownRefresh: function() {
+    //   // 触发下拉刷新时执行
+    // },
+    // onReachBottom: function() {
+    //   // 页面触底时执行
+    // },
+    // onShareAppMessage: function () {
+    //   // 页面被用户分享时执行
+    // },
+    // onPageScroll: function() {
+    //   // 页面滚动时执行
+    // },
+    // // 事件响应函数
+    // viewTap: function() {
+    //   this.setData({
+    //     text: 'Set some data for updating view.'
+    //   }, function() {
+    //     // this is setData callback
+    //   })
+    // }
+})
