b60e0c7c8e05dd7f495e08940048ddb1f8aeec3c
Hotdog <1963335234@qq.com>
2021-04-10 20:43:27 +0800

    周六晚

---
+++ pages/page/more/data.js
@@ -0,0 +1,42 @@
+let list_data = [{
+  activID:'000001',
+  activName:'什么活动啊',
+  activPlace:'什么地方啊',
+  activTime:'20xx-xx-xx',
+  activTag:'什么标签啊',
+  activIni:'谁啊',
+  activDes:'数据全都放这个文件里，页面不用管自动遍历',
+  },
+  {
+    activID:'000002',
+    activName:'什么活动啊',
+    activPlace:'什么地方啊',
+    activTime:'20xx-xx-xx',
+    activTag:'什么标签啊',
+    activIni:'谁啊',
+  activDes:'有什么描述啊有什么描述啊什么描述啊什么描述啊有什么描述啊什么描述啊什么描述啊',
+
+  },
+  {
+    activID:'000003',
+    activName:'什么活动啊',
+    activPlace:'什么地方啊',
+    activTime:'20xx-xx-xx',
+    activTag:'什么标签啊',
+    activIni:'谁啊',
+  activDes:'有什么描述啊什么描述啊什么描述啊什么描述啊有什么描述啊什么描述啊什么描述啊',
+
+  },
+  {
+    activID:'000004',
+    activName:'什么活动啊',
+    activPlace:'什么地方啊',
+    activTime:'20xx-xx-xx',
+    activTag:'什么标签啊',
+    activIni:'谁啊',
+  activDes:'有什么描述啊什么描述啊什么描述啊什么描述啊有什么描述啊什么描述啊什么描述啊',
+  }
+]
+module.exports = {
+  list_data
+}
--- pages/page/more/more.js
+++ pages/page/more/more.js
@@ -1,9 +1,18 @@
+let datas=require('./data.js');
 Page({
     data: {
-  
+      listArry:[],
+      color: ''
     },
     onLoad: function () {
-
+      this.setData({
+        listArry:datas.list_data
+      })
     },
-  })
-  
+    input:function(){
+    this.setData({color:'#6495ED'})
+    },
+    uninput:function(){
+    this.setData({color:'#FFA500'})
+    }
+})
